efd1f383e88fe36205ee129be87617732ad7a747
Chance Harmon <henrikillsit@gmail.com>
2021-11-08 22:30:01 -0800

    very basic react front end done for chat box

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+import './App.css';
+import io from 'socket.io-client';
+import { useState } from 'react';
+import Chat from './Chat.js'
+
+
+// const socket = io.connect('http://localhost:3001')
+// Switch of local versus live deployment
+const socket = io.cconnect('https://chances-socket-server.herokuapp.com/')
+
+function App() {
+
+  const [userName, setUserName] = useState('');
+  const [roomName, setRoomName] = useState('');
+  const [joinedRoom, setJoinedRoom] = useState(false);
+  const joinRoom = () => {
+    if (userName !== '' && roomName !== '') {
+      socket.emit('join_room', roomName)
+      setJoinedRoom(true)
+    }
+  }
+
+  return (
+    <div className="App">
+      <h3>Welcome to my chat box</h3>
+      {!joinedRoom ?
+        (<div>
+          <input type='text' placeholder='Enter your name' onChange={(e) => setUserName(e.target.value)} />
+          <input type='text' placeholder='Enter Room Name' onChange={(e) => setRoomName(e.target.value)} />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>) :
+        <Chat socket={socket} userName={userName} roomName={roomName} />
+
+      }
+
+
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/Chat.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import './Chat.css'
+
+function Chat({ socket, userName, roomName }) {
+
+  const [currentMessage, setCurrentMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = async () => {
+    if (currentMessage !== '') {
+      const messageData = {
+        roomName,
+        userName,
+        currentMessage,
+        time: `${new Date(Date.now()).getHours()}:${new Date(Date.now()).getMinutes()}`
+      }
+
+      await socket.emit('send_message', messageData)
+      console.log('in send', messageData, messageList)
+      setMessageList([...messageList, messageData]);
+      setCurrentMessage('')
+    }
+  }
+
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      console.log(typeof data, data)
+      setMessageList([...messageList, data])
+    })
+  })
+
+  return (
+    <div>
+      <div className='chat-header'>
+        <p>You are in the {roomName}</p>
+      </div>
+      <div className='chat-body'>
+        {messageList.map((message, i) => {
+          return <div key={i} id={message.userName === userName ? 'my-message' : 'others-message'}>
+            <p>{message.currentMessage}</p>
+            <p>Sent by: {message.userName}</p>
+            <p>at: {message.time}</p>
+          </div>
+        })}
+      </div>
+      <div className='chat-footer'>
+        <input type='text' placeholder='Type a message' value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} />
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  )
+}
+
+export default Chat;
